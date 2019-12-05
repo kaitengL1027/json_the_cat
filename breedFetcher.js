dfeade44cad21e08bff714babebc929a6f5ea031
Kaiteng Lo <kkktkaten@hotmail.com>
2019-12-05 20:37:55 +0000

    cat api things added

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,21 @@
 const request = require('request');
-const args = process.argv;
-let input = args.slice(2);
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + input[0], (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  if (body === '[]') {
-    console.log("Breed Not Found!");
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0]);
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
 
-});
+    if (error) {
+      callback(error);
+    }
+
+    if (body === '[]') {
+      callback("Breed Not Exist");
+    } 
+    
+    else {
+      const data = JSON.parse(body);
+      callback(null, String(data[0].description));
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
